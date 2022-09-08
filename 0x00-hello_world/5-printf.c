c70dda1fdf183bbfc187e3440b31053876ee9387
alex <alexmuthini3@gmail.com>
2022-09-08 22:40:33 +0300

    resolved errors in file 1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - entry point
- * Returns: Always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
+
