3cd098ccb98b261e2333a6d88d94cae8629c68cd
alex <alexmuthini3@gmail.com>
2022-09-09 12:34:42 +0300

    corrected betty style errors 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,18 +17,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 	{
-		printf("%d %s\n",n,"is positive");
+		printf("%d %s\n", n, "is positive");
 	}
 	else if (n < 0)
 	{
-		printf("%d %s\n",n,"is negative");
+		printf("%d %s\n", n, "is negative");
 	}
 	else
 	{
-		printf("%d %s\n",n,"is zero");
+		printf("%d %s\n", n, "is zero");
 	}
 
 	return (0);
