21cbae05b3963419703a3bda44885db4fbad9c4e
alex <alexmuthini3@gmail.com>
2022-09-09 15:20:10 +0300

    added 7-print-tabahpla.c file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
