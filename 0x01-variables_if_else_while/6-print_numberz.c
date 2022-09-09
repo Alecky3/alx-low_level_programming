9acfeacb790d8bc97d82786a55a4181dca031901
alex <alexmuthini3@gmail.com>
2022-09-09 15:10:56 +0300

    modified file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
