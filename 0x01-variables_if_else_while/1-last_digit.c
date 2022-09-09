8399a16318dcc64c559277b63bdd32ed7b527366
alex <alexmuthini3@gmail.com>
2022-09-09 13:27:34 +0300

    correcting betty identation problems 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,17 +17,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lg=n % 10;
+	lg = n % 10;
 
-       if (lg > 5 )
+	if (lg > 5)
        {
 	       printf("Last digit of %d is %d and is greater than 5\n", n, lg);
        }
 
-       else if (lg == 0){
+       else if (lg == 0)
+       {
 	       printf("Last digit of %d is %d and is 0\n", n, lg);
        }
-       else if (n < 6 && n != 0){
+       else if (n < 6 && n != 0)
+       {
 	       printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lg);
        }
 
