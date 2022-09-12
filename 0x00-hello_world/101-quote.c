7f510f302a7376c69313c49532756b2eae3a6c7d
alex <alexmuthini3@gmail.com>
2022-09-12 13:00:40 +0300

    added 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - programs entry point
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19 ,\n");
+
+	return (1);
+}
