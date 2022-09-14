0766a85ea869416d943585da44d03f58fc621e86
alex <alexmuthini3@gmail.com>
2022-09-14 16:59:15 +0300

    added use of putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - programs entry point
+ * main - prints _putchar
  * Return: 0 Success
  */
 
+
 int main(void)
 {
-	printf("_putchar\n");
+	int str[] = {45, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
 
 	return (0);
 }
