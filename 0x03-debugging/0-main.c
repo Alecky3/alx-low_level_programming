9bfb2a285fcf45cdc2fd83534b9d93e8f70102b6
alex <alexmuthini3@gmail.com>
2022-09-15 16:52:26 +0300

    added main.h header file and 0-main.c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - tests function that checks if integer is positive or negative
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	
+	positive_or_negative(0);
+
+	return (0);
+}
