e14106a285feee89899aed2705687e1fd3f943af
alex <alexmuthini3@gmail.com>
2022-09-15 17:01:25 +0300

    modifed main. h header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -14,4 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif
+#endif /* MAIN_H */
