192d19faf5584ac87f8c16c92ab6955f39241eab
alex <alexmuthini3@gmail.com>
2022-09-19 15:32:39 +0300

    added 7-puts_half.c file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints the last half of the parameter string
+ * @str: the string to print
+ * Return: none
+ */
+
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int mid = (len - 1) / 2;
+	int i = mid;
+
+	while (i < len)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
