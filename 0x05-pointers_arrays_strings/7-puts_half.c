f487c392d3ba0c08651497d1bb3b31480e6b5817
alex <alexmuthini3@gmail.com>
2022-09-19 15:41:16 +0300

    corrected logical errors

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,7 +10,13 @@
 void puts_half(char *str)
 {
 	int len = strlen(str);
-	int mid = (len - 1) / 2;
+	int mid;
+
+	if (mid % 2 == 0)
+		mid = len / 2;
+	else
+		mid = (len - 1) / 2;
+
 	int i = mid;
 
 	while (i < len)
