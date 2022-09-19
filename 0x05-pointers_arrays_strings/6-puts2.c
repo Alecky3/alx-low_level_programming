6f17d9793d0c662144b0b408e60debbabeaf06a8
alex <alexmuthini3@gmail.com>
2022-09-19 15:16:45 +0300

    added 6-puts2.c file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main"
+#include <string.h>
+
+/**
+ * puts2 - prints every 2 character of a string
+ * @str: the string to print
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int len = strlen(str), i;
+
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
