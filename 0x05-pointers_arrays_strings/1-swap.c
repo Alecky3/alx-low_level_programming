75aca479b3919f6f569524633afc5ee27b83f8bc
alex <alexmuthini3@gmail.com>
2022-09-19 13:11:26 +0300

    added 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - swaps the values of the parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: void
+ */
+
+void swap_int(int *a, int *b){
+
+	int *temp = a;
+	*a = *b;
+	*b = temp;
+}
