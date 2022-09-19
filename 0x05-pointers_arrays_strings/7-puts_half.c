12b0446011e3d0a5b8af2e53c7130272f75210cf
alex <alexmuthini3@gmail.com>
2022-09-19 15:43:38 +0300

    corrected compilation errors

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,14 +10,14 @@
 void puts_half(char *str)
 {
 	int len = strlen(str);
-	int mid;
+	int mid, i;
 
 	if (mid % 2 == 0)
 		mid = len / 2;
 	else
 		mid = (len - 1) / 2;
 
-	int i = mid;
+	i = mid;
 
 	while (i < len)
 	{
