b44ae0145fd791e72d841e328df1f609c1ec9699
alex <alexmuthini3@gmail.com>
2022-09-26 15:46:01 +0300

    added _strchr function prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,4 +2,5 @@
 #define _MAIN_H
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif /** end of header file */
