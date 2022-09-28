f6f37c4e26ad994cfa573046b1f0610c76e2d229
alex <alexmuthini3@gmail.com>
2022-09-28 17:43:54 +0300

    added _sqrt_recursion prototype

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,15 +8,18 @@
 
 int is_prime_number(int n)
 {
-	static int i = 2;
-
-	if (n <= 2)
-		return ((n == 2) ? (1) : (0));
-	if (n % i == 0)
-		return (0);
-	if (i * i > n)
-		return (1);
-	i++;
-	return (is_prime_number(n));
+	if(i==1)
+    {
+        return 1;
+    }
+    else if(n1 %i==0)
+    {
+         return 0;
+    }
+    else
+       {
+         i = i -1;
+         is_prime_number(n);
+      }
 
 }
