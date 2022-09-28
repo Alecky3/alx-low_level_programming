1928cfc2d68968922b19435d7bd27fc469fbb889
alex <alexmuthini3@gmail.com>
2022-09-28 15:40:46 +0300

    added 1-print_rev_recursion.c file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints parameter string using recursion
+ * in reverse order
+ * @s: the string to print;
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_puts_recursion(s + 1);
+	_putchar(*s);
+}
