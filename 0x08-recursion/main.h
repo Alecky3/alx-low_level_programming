2fa369ac592182eba74890f4b42f17db590a2ae3
alex <alexmuthini3@gmail.com>
2022-09-28 18:03:21 +0300

    corrected compile time errors v2

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,4 +9,5 @@ int _pow_recursion(int x, int y);
 int is_prime_number(int n);
 int _sqrt_recursion(int n);
 int power_operation(int n, int c);
+int is_prime(unsigned int n, unsigned int c);
 #endif /* end of _MAIN_H header file */
