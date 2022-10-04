a0a7e0eaaaad9b4195a3cc14cd5877a83ef5b337
alex <alexmuthini3@gmail.com>
2022-10-04 16:11:26 +0300

    corrected compile errors v1

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
-			*(ptr + i * height + j) = 0;
+			*(ptr_int + i * height + j) = 0;
 	}
 
 	return (ptr_int);
