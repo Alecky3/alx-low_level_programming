207e2a60d2100f42b3260e6a43e47022894b77d1
alex <alexmuthini3@gmail.com>
2022-10-04 15:43:50 +0300

    corrected compile errors v2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	else if (s2 == NULL)
-		len2 = "";
+		s2 = "";
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	ptr_concat = malloc((len1 + len2) * sizeof(char) + 1);
