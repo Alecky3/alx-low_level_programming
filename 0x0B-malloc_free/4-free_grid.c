db4243b84ae1be5142e5616e527297596df4a7ff
alex <alexmuthini3@gmail.com>
2022-10-04 17:06:20 +0300

    corrected betty-doc errors

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * free_grid frees a 2 dimensional grid previously created by alloc_grid
+ * free_grid - frees a 2 dimensional grid previously created by alloc_grid
  * function
  * @grid: the grid to free
  * @height: geight of the grid
