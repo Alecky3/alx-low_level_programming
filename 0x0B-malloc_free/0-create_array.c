3ec36cc9b6c29dad874b47edc968f0a592ac53be
alex <alexmuthini3@gmail.com>
2022-10-04 14:54:04 +0300

    corrected logical errors v2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	chars = malloc(size * sizeof(char) + 1);
+	chars = malloc(size * sizeof(char));
 	if (chars == NULL)
 		return (NULL);
 
