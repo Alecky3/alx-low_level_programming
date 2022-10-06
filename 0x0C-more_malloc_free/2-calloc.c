05ef8bdd6398c68d8b0fa2181e884f6336d62e31
alex <alexmuthini3@gmail.com>
2022-10-06 15:02:00 +0300

    added 2-calloc.c file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.c"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements of the array
+ * @size: size in bytes of each element
+ * Return: pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr_ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr_ptr = malloc(nmemb * size);
+
+	if (arr_ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		arr_ptr[i] = 0;
+
+	return (arr_ptr);
+}
