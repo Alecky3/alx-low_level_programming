7b20d6497c57d7dd30deae1a937af8b510c8ac07
alex <alexmuthini3@gmail.com>
2022-10-12 16:12:41 +0300

    corrected compile errors v2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 		if (!(d->age))
-			printf("Age: (nil)\n")
+			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
 
