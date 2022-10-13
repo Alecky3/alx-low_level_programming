6a157938a10d1f5ee0e4beb1ef4dc536bf2b0993
alex <alexmuthini3@gmail.com>
2022-10-13 14:50:01 +0300

    added array_iterator prototype

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,5 @@
 #define _FUNCTION_POINTERS_H_
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /** end of _FUNCTION_POINTERS_H_ **/
