880d6035693fcff9a470277ca94e9aa3ffed05e3
alex <alexmuthini3@gmail.com>
2022-10-13 15:24:07 +0300

    corrected compile errors v1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * int_index - searches for an integer
@@ -14,14 +15,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, n = size, res = -1;
 
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < n; i++)
-	{
-		if (cmp(array[i]))
-			return (i);
-	}
+	if (array && cmp)
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < n; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 
 	return (res);
 }
