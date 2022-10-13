104a4cf300ac931253dd72b3bc87ab713cbff42b
alex <alexmuthini3@gmail.com>
2022-10-13 14:41:17 +0300

    added 0-print_name.c file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the parameter string
+ * @name: the name to print
+ * @f: the real function pointer to print the name
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
