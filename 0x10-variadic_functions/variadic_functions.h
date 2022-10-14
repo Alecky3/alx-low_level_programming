5802932415da2b1187730a50888c99c1385d73f8
alex <alexmuthini3@gmail.com>
2022-10-14 11:59:04 +0300

    added print_numbers prototype

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,5 +3,5 @@
 #include <stdarg.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif /** end of _VARIADIC_FUNCTIONS_H_ **/
