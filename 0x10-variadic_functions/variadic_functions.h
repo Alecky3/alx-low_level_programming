6429fc19438627b7fed1da089a37c63c2dfd4837
alex <alexmuthini3@gmail.com>
2022-10-14 11:30:15 +0300

    added variadic_functions.h header and sum_them_all prototype

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif /** end of _VARIADIC_FUNCTIONS_H_ **/
