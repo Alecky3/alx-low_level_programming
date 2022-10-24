976c1586f9c66d0a4ae1a1d93ed716f64f7d8845
alex <alexmuthini3@gmail.com>
2022-10-24 21:25:29 +0300

    added list_len prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,6 +18,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 #endif /** end of _LISTS_H_ **/
