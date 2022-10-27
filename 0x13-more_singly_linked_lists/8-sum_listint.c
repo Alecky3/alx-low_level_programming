c148f175a6951043d643d5e710b7513404f5b03d
alex <alexmuthini3@gmail.com>
2022-10-27 17:43:38 +0300

    corrected betty-style errors

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all the data (n) of a listint_t 
+ * sum_listint - returns the sum of all the data (n) of a listint_t
  * linked list.
  * @head: head of a list.
  * Return: sum of all the data (n) or 0 if empty
