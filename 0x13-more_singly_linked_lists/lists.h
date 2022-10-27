39295502f740d6d62fb8e1d74f346c225b51c8b4
alex <alexmuthini3@gmail.com>
2022-10-27 16:58:22 +0300

    added listint_t prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,21 @@
 #define _LISTS_H_
 #include <stdlib.h>
 #include <unistd.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 #endif /** end of _LISTS_H_ **/
