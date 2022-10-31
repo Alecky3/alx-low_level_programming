ae34ea79ef7a95980d365e4c15c5280ed92c55a0
alex <alexmuthini3@gmail.com>
2022-10-31 16:35:42 +0300

    corrected logical errors v1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,10 +8,15 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if (n >> 0)
 	{
-		print_binary(n / 2);
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
 	}
 
-	n % 2 == 0 ? _putchar('0') : _putchar('1');
 }
