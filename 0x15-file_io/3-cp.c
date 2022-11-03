2b38749037dfb998cdae5078e899e1639e138334
alex <alexmuthini3@gmail.com>
2022-11-03 21:09:48 +0300

    corrected compile errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
 
 /**
  * main - check the code for Holberton School students.
@@ -10,64 +30,44 @@
  */
 int main(int argc, char *argv[])
 {
-	int from, to, err_close;
+	int file_from, file_to, err_close;
 	ssize_t nchars, nwr;
 	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp from to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	from = open(argv[1], O_RDONLY);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	check_file_error(from, to, argv);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
 	nchars = 1024;
 	while (nchars == 1024)
 	{
-		nchars = read(from, buf, 1024);
+		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
-			check_file_error(-1, 0, argv);
-		nwr = write(to, buf, nchars);
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
-			check_file_error(0, -1, argv);
+			error_file(0, -1, argv);
 	}
 
-	err_close = close(from);
+	err_close = close(file_from);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	err_close = close(to);
+	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
 }
 
-/**
- * check_file_error - checks for errors in open from and to files
- * @from: file read from.
- * @to: file writen to..
- * @argv: contains the name of the files.
- * Return: void
- */
-void check_file_error(int from, int to, char *argv[])
-{
-	if (from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-}
