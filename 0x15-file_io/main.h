b94b26c46adf3f260e8ca4a4fb2015c50e4d4c39
alex <alexmuthini3@gmail.com>
2022-11-03 12:42:38 +0300

    added create_file prototype

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,4 +7,5 @@
 #include <sys/fcntl.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif /* end of _MAIN_H_ header */
