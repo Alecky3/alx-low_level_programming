23e362c79f21465326c878e59de54e6c8d281733
alex <alexmuthini3@gmail.com>
2022-11-03 12:27:01 +0300

    added main.h header

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+#endif /* end of _MAIN_H_ header */
