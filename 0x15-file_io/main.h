9e68cb92af467fa878867e5dd29dd1020629faa1
alex <alexmuthini3@gmail.com>
2022-11-03 13:23:39 +0300

    added check_file_error prototype

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,4 +9,5 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void check_file_error(int from, int to, char *argv[]);
 #endif /* end of _MAIN_H_ header */
