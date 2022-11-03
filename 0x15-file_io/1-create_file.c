97cecde01cf79c62f572093aefa840bb35a7eee6
alex <alexmuthini3@gmail.com>
2022-11-03 12:51:04 +0300

    added 1-create_file.c file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create.
+ * @text_content: NULL terminated string to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int count;
+	int charsWiten;
+
+	count = strlen(text_content);
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	charsWiten = write(fd, text_content, count);
+
+	if (charsWiten == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
