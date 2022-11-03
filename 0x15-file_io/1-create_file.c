57143d82b36880e13e47e51b17661ad0a9f3a8fe
alex <alexmuthini3@gmail.com>
2022-11-03 13:00:19 +0300

    corrected logical errors v2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,10 @@ int create_file(const char *filename, char *text_content)
 	int count;
 	int charsWiten;
 
+	if (!text_content)
+		text_content = "";
 	count = strlen(text_content);
+
 	if (!filename)
 		return (-1);
 
@@ -22,8 +25,6 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
 
 	charsWiten = write(fd, text_content, count);
 
