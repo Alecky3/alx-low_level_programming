81d21466899092e4cb24e79eed682e4341681860
alex <alexmuthini3@gmail.com>
2022-12-08 16:52:40 +0300

    corrected logical errors v3

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,24 +1,26 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in a linked list
+ * dlistint_len - returns the number of elements in doubly linked list.
  * @h: head of the list
- * Return: number of elemnts
+ * Return: the number of elements in the list.
  */
-
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count = 0;
+	int count;
+
+	count = 0;
 
 	if (h == NULL)
 		return (count);
-        while (h->prev != NULL)
-	{
+
+	while (h->prev != NULL)
 		h = h->prev;
-	}
+
 	while (h != NULL)
 	{
-		count += 1;
+		count++;
+		h = h->next;
 	}
 
 	return (count);
