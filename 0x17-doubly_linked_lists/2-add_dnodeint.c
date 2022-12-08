973a6aab7e0c0c2925be078270c5439d02cf3f9d
alex <alexmuthini3@gmail.com>
2022-12-08 15:03:24 +0300

    corrected compile errors v1

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,11 +21,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->next = *head;
 	new_node->prev = NULL;
 
-	if (*head != NULL)
+	if ((*head) != NULL)
 	{
-		*head->prev = new_node;
+		(*head)->prev = new_node;
 	}
-	*head = new_node;
+	(*head) = new_node;
 
 	return (new_node);
 }
