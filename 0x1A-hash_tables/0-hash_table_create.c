053c0acf2c8a810a1abb50dc82b77fa7ce8cdd52
alex <alexmuthini3@gmail.com>
2023-01-08 22:17:44 +0300

    added calloc allocation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (hashtable == NULL)
 		return (NULL);
 	hashtable->size = size;
-	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+	hashtable->array = calloc(size, sizeof(hash_node_t *));
 	if (hashtable->array == NULL)
 		return (NULL);
 
