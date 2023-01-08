4812c2cef194fbeeaa5af166688e1c6ee614b03a
alex <alexmuthini3@gmail.com>
2023-01-08 22:27:33 +0300

    corrected requirement fail v1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,19 +1,20 @@
 #include "hash_tables.h"
 
 /**
- * hash_key_key_djb2 - Hash function that implement djb2 algorithm.
+ * hash_djb2 - Hash function that implement djb2 algorithm.
  * @str: Key to aplied the transformation.
- * Return: hash_key_key key
+ *
+ * Return: Integer after the operations.
  */
-unsigned long int hash_key_key_djb2(const unsigned char *str)
+unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash_key;
+	unsigned long int hash;
 	int c;
 
-	hash_key = 5381;
+	hash = 5381;
 	while ((c = *str++))
 	{
-		hash_key = ((hash_key << 5) + hash_key) + c; 
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
-	return (hash_key);
+	return (hash);
 }
