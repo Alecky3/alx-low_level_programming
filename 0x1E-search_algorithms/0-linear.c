1489b67e6c7b0987363f34a6426edb17cfae4286
alex <alexmuthini3@gmail.com>
2023-04-12 20:40:55 +0300

    corrected betty style errors in 0-linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,7 +3,7 @@
  * linear_search - searches  for a value in an array of integers using the
  * linear serach algorithm
  * @array: the array to search
- * @sze: size of the array
+ * @size: size of the array
  * @value: the values we are searching
  * Return: first index where value is located else if value is not present
  * in the array or array is NULL return -1
@@ -17,7 +17,6 @@ int linear_search(int *array, size_t size, int value)
 	{
 		return (-1);
 	}
-	
 
 	for (i = 0; i < size; i++)
 	{
